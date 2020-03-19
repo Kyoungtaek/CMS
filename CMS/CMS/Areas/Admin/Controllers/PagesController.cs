e9f784ef8b9f66848f698cc6d669baae9f8be19b
Kyoungtaek Oh <okts83@gmail.com>
2020-03-18 21:45:31 -0400

    13.Admin Pages Index Controller

--- CMS/CMS/Areas/Admin/Controllers/PagesController.cs
+++ CMS/CMS/Areas/Admin/Controllers/PagesController.cs
@@ -2,16 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CMS.Infrastructure;
+using CMS.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CMS.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class PagesController : Controller
     {
-        public string Index()
+        private readonly CmsContext context;
+        public PagesController(CmsContext context)
         {
-            return "test";
+            this.context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IQueryable<Page> pages = from p in context.Pages orderby p.Sorting select p;
+
+            List<Page> pagesList = await pages.ToListAsync();
+
+            return View(pagesList);
         }
     }
 }
