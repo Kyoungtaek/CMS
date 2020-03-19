50ce0feb353aa5e260a216edb3dd2fd4e9d675a4
Kyoungtaek Oh <okts83@gmail.com>
2020-03-18 21:34:28 -0400

    12.Admin area and Page Congroller

--- CMS/CMS/Startup.cs
+++ CMS/CMS/Startup.cs
@@ -54,6 +54,10 @@ namespace CMS
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                  );
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
