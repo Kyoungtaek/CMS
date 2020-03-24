723bf1d1bf848f891c7ce4f5dfee386f71406fe7
Kyoungtaek Oh <okts83@gmail.com>
2020-03-24 14:37:11 -0400

    34. Admin Products controller, model and migration

---
+++ CMS/CMS/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CMS.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly CmsContext context;
+        public ProductsController(CmsContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
