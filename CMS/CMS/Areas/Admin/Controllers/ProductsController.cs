b2954816f5f5abd1422795a554a5c0028183bcea
Kyoungtaek Oh <okts83@gmail.com>
2020-03-25 12:52:56 -0400

    45. Admin Products Details

--- CMS/CMS/Areas/Admin/Controllers/ProductsController.cs
+++ CMS/CMS/Areas/Admin/Controllers/ProductsController.cs
@@ -91,5 +91,18 @@ namespace CMS.Areas.Admin.Controllers
 
             return View(product);
         }
+
+        // GET /admin/products/details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            Product product = await context.Products.Include(x => x.Category).FirstOrDefaultAsync(x => x.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
     }
 }
