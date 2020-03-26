05783cbd1b9c451641cec24bbe06309c90055b94
Kyoungtaek Oh <okts83@gmail.com>
2020-03-26 12:08:09 -0400

    52. Products Controller and Index

---
+++ CMS/CMS/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CMS.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMS.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly CmsContext context;
+
+        public ProductsController(CmsContext context)
+        {
+            this.context = context;
+        }
+
+        // GET /products
+        public async Task<IActionResult> Index(int p = 1)
+        {
+            int pageSize = 6;
+            var products = context.Products.OrderByDescending(x => x.Id).Include(x => x.Category)
+                                    .Skip((p - 1) * pageSize)
+                                    .Take(pageSize);
+
+            ViewBag.PageNumber = p;
+            ViewBag.PageRange = pageSize;
+            ViewBag.TotalPages = (int)Math.Ceiling((decimal)context.Products.Count() / pageSize);
+
+            return View(await products.ToListAsync());
+        }
+    }
+}
