a820740237c02bee4a13a2d599d90d0dca9a3d3e
Kyoungtaek Oh <okts83@gmail.com>
2020-03-26 11:59:33 -0400

    51. Main Menu View Component

---
+++ CMS/CMS/Infrastructure/MainMenuViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using CMS.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS.Infrastructure
+{
+    public class MainMenuViewComponent: ViewComponent
+    {
+        private readonly CmsContext context;
+
+        public MainMenuViewComponent(CmsContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var pages = await GetPagesAsync();
+
+            return View(pages);
+        }
+
+        private Task<List<Page>> GetPagesAsync()
+        {
+            return context.Pages.OrderBy(x => x.Sorting).ToListAsync();
+        }
+    }
+}
