64224d0c81b6e6aaccb1155141aed59eb83d4784
Kyoungtaek Oh <okts83@gmail.com>
2020-03-26 21:31:40 -0400

    58. Cart Index

--- CMS/CMS/Controllers/CartController.cs
+++ CMS/CMS/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CMS.Infrastructure;
+using CMS.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CMS.Controllers
@@ -16,8 +17,16 @@ namespace CMS.Controllers
             this.context = context;
         }
 
+        // GET /cart
         public IActionResult Index()
         {
+            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart") ?? new List<CartItem>();
+
+            CartViewModel cartVM = new CartViewModel
+            {
+                CartItems = cart,
+                GrandTotal = cart.Sum(x => x.Price * x.Quantity)
+            };
             return View();
         }
     }
