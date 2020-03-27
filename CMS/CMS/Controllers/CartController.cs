0e7f584a03fa412a7eed87a4479b67b8170f4382
Kyoungtaek Oh <okts83@gmail.com>
2020-03-26 21:22:24 -0400

    56. Cart Controller and SessionExtensions class

---
+++ CMS/CMS/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CMS.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly CmsContext context;
+
+        public CartController(CmsContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
