2888b869179857323cfb15d310d4a89cf1413eea
Kyoungtaek Oh <okts83@gmail.com>
2020-03-29 14:59:30 -0400

    71. Checkout 2

--- CMS/CMS/Controllers/CartController.cs
+++ CMS/CMS/Controllers/CartController.cs
@@ -114,7 +114,12 @@ namespace CMS.Controllers
 
             //return RedirectToAction("Page", "Pages");
             //return Redirect("/");
-            return Redirect(Request.Headers["Referer"].ToString());
+            if (HttpContext.Request.Headers["X-Requested-With"] != "XMLHttpRequest")
+            {
+                return Redirect(Request.Headers["Referer"].ToString());
+            }
+
+            return Ok();
         }
     }
 }
