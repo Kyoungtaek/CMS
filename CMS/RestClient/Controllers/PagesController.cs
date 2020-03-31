740f4bd67fc072302dd9a9c536cf55d60f2df5c6
Kyoungtaek Oh <okts83@gmail.com>
2020-03-31 17:17:51 -0400

    105. RestClient Pages Index and view

--- CMS/RestClient/Controllers/PagesController.cs
+++ CMS/RestClient/Controllers/PagesController.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestClient.Models;
 
 namespace RestClient.Controllers
 {
     public class PagesController : Controller
     {
-        public IActionResult Index()
+        // GET pages
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<Page> pages = new List<Page>();
+
+            using(var httpClient = new HttpClient())
+            {
+                using var request = await httpClient.GetAsync("https://localhost:44348/api/pages");
+                string response = await request.Content.ReadAsStringAsync();
+
+                pages = JsonConvert.DeserializeObject<List<Page>>(response);
+            }
+
+            return View(pages);
         }
     }
 }
