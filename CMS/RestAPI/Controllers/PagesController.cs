3b36a9adb46bf2741e26b88a576b4fc95e2c38a1
Kyoungtaek Oh <okts83@gmail.com>
2020-03-31 15:46:32 -0400

    97. RestAPI Pages model and controller and database connection

---
+++ CMS/RestAPI/Controllers/PagesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestAPI.Models;
+
+namespace RestAPI.Controllers
+{
+    public class PagesController : ControllerBase
+    {
+        private readonly CmsContext context;
+
+        public PagesController(CmsContext context)
+        {
+            this.context = context;
+        }
+    }
+}
---
+++ CMS/RestAPI/Models/CmsContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPI.Models
+{
+    public class CmsContext: DbContext
+    {
+        public CmsContext(DbContextOptions<CmsContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Page> Pages { get; set; }
+    }
+}
