d07d2ed007fa934f36d9b9d3d085ae4b3d3ab211
Kyoungtaek Oh <okts83@gmail.com>
2020-03-31 16:04:34 -0400

    98. RestAPI Get Pages

--- CMS/RestAPI/Controllers/PagesController.cs
+++ CMS/RestAPI/Controllers/PagesController.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RestAPI.Models;
 
 namespace RestAPI.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class PagesController : ControllerBase
     {
         private readonly CmsContext context;
@@ -14,6 +17,11 @@ namespace RestAPI.Controllers
         public PagesController(CmsContext context)
         {
             this.context = context;
+        }
+
+        public async Task<ActionResult<IEnumerable<Page>>> GetPages()
+        {
+            return await context.Pages.OrderBy(x => x.Sorting).ToListAsync();
         }
     }
 }
