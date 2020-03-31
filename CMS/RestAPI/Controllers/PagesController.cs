f7c47358c7469524d6b21f967be14234f1e39a9f
Kyoungtaek Oh <okts83@gmail.com>
2020-03-31 16:08:59 -0400

    99. RestAPI Get Page

--- CMS/RestAPI/Controllers/PagesController.cs
+++ CMS/RestAPI/Controllers/PagesController.cs
@@ -19,9 +19,25 @@ namespace RestAPI.Controllers
             this.context = context;
         }
 
+        // GET /api/pages
+        [HttpGet]
         public async Task<ActionResult<IEnumerable<Page>>> GetPages()
         {
             return await context.Pages.OrderBy(x => x.Sorting).ToListAsync();
+        }
+
+        // GET /api/pages/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Page>> GetPage(int id)
+        {
+            var page = await context.Pages.FindAsync(id);
+
+            if (page == null)
+            {
+                return NotFound();
+            }
+
+            return page;
         }
     }
 }
